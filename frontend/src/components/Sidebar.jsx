dc2b0b5f6067a184a19e27ec03495c46bd1df960
Aditya Joshi <joshiaditya.aj07@gmail.com>
2023-08-04 19:33:56 +0530

    fix: dashboard page layout fix

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react";
 import logo from "../assets/HealthSynx.png";
 import SidebarProfile from "../components/SidebarProfile";
 import { BiSolidDashboard } from "react-icons/bi";
@@ -11,78 +11,72 @@ import { LuLogOut } from "react-icons/lu";
 import { Link } from "react-router-dom";
 
 const menuLinks = [
-    {
-      title: "Dashboard",
-      icon: BiSolidDashboard,
-    },
-    {
-      title: "Activity",
-      icon: FiActivity,
-    },
-    {
-      title: "Goals",
-      icon: GoGoal,
-    },
-    {
-      title: "Nutrition",
-      icon: GiFruitBowl,
-    },
-    {
-      title: "Wearables",
-      icon: TbDeviceWatchHeart,
-    },
-    {
-      title: "Settings",
-      icon: FiSettings,
-    },
-  ];
+  {
+    title: "Dashboard",
+    icon: BiSolidDashboard,
+  },
+  {
+    title: "Activity",
+    icon: FiActivity,
+  },
+  {
+    title: "Goals",
+    icon: GoGoal,
+  },
+  {
+    title: "Nutrition",
+    icon: GiFruitBowl,
+  },
+  {
+    title: "Wearables",
+    icon: TbDeviceWatchHeart,
+  },
+  {
+    title: "Settings",
+    icon: FiSettings,
+  },
+];
 
 const Sidebar = () => {
   return (
-<>
-        <div className="flex flex-col gap-4 py-6 px-4 ">
-          <div className="logo flex gap-3 items-center justify-center border-b-[1px] pb-4">
-            <img
-              src={logo}
-              alt="logo"
-              className="rounded-md w-[3rem] h-[3rem]"
-            />
-            <span className="text-2xl font-extrabold text-white">
-              HealthSynx.
-            </span>
-          </div>
+    <div className="fixed top-0 left-0 h-screen rounded-r-3xl z-40 w-[30%] lg:w-[20%] bg-black ">
+      <div className="flex flex-col gap-4 py-6 px-4 ">
+        <div className="logo flex gap-3 items-center justify-center border-b-[1px] pb-4">
+          <img src={logo} alt="logo" className="rounded-md w-[3rem] h-[3rem]" />
+          <span className="text-2xl font-extrabold text-white">
+            HealthSynx.
+          </span>
         </div>
+      </div>
 
-        <div className="max-w-[90%] mx-auto flex flex-col justify-between">
-          <SidebarProfile />
-          <ul className="flex flex-col items-center justify-center gap-4 text-white mt-12 w-full">
-            {menuLinks.map((link) => (
-              <Link
-                to={`/dashboard${
-                  link.title === "Dashboard"
-                    ? ""
-                    : "/" + link.title.toLowerCase()
-                }`}
-                className="cursor-pointer rounded-lg hover:bg-gray-400 hover:bg-opacity-20 transition-colors duration-200 w-full gap-4"
+      <div className="max-w-[90%] mx-auto flex flex-col justify-between">
+        <SidebarProfile />
+        <ul className="flex flex-col items-center justify-center gap-4 text-white mt-12 w-full">
+          {menuLinks.map((link) => (
+            <Link
+              to={`/dashboard${
+                link.title === "Dashboard" ? "" : "/" + link.title.toLowerCase()
+              }`}
+              className="cursor-pointer rounded-lg hover:bg-gray-400 hover:bg-opacity-20 transition-colors duration-200 w-full gap-4"
+            >
+              <li
+                key={link.title}
+                className="flex justify-start items-center p-2 md:px-2 lg:px-4  w-full gap-4"
               >
-                <li
-                  key={link.title}
-                  className="flex justify-start items-center p-2 md:px-2 lg:px-4  w-full gap-4"
-                >
-                  <span>{<link.icon size={20} />}</span>
-                  <span>{link.title}</span>
-                </li>
-              </Link>
-            ))}
-          </ul>
+                <span>{<link.icon size={20} />}</span>
+                <span>{link.title}</span>
+              </li>
+            </Link>
+          ))}
+        </ul>
 
-          <button className="flex justify-start items-center gap-4 text-white ounded-lg hover:bg-gray-400 hover:bg-opacity-20 transition-colors duration-200 p-2 md:px-2 lg:px-4 cursor-pointer rounded-lg mt-48 ">
-            <LuLogOut size={20} />
-            Log out
-          </button>
-        </div>
-        </>
-  )
-}
+        <button className="flex justify-start items-center gap-4 text-white ounded-lg hover:bg-gray-400 hover:bg-opacity-20 transition-colors duration-200 p-2 md:px-2 lg:px-4 cursor-pointer rounded-lg mt-48 ">
+          <LuLogOut size={20} />
+          Log out
+        </button>
+      </div>
+    </div>
+  );
+};
 
-export default Sidebar
+export default Sidebar;
--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -2,14 +2,13 @@ import React from "react";
 import Sidebar from "../components/Sidebar";
 import { Outlet } from "react-router-dom";
 
-
 const Dashboard = () => {
   return (
     <div className="flex">
-      <div className="fixed top-0 left-0 h-screen rounded-r-3xl z-40 w-[20%] bg-black ">
-        <Sidebar />
+      <div className="w-[30%] lg:w-[20%]">
+      <Sidebar />
       </div>
-      <div className="h-screen w-full bg-white ">
+      <div className="h-screen md:w-[70%] lg:w-[80%] bg-white ">
         <Outlet></Outlet>
       </div>
     </div>
