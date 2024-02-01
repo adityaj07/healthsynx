fe805135826f0f55e1b2742e475a97a740a2413b
Aditya Joshi <joshiaditya.aj07@gmail.com>
2024-02-01 12:58:10 +0530

    fix: middleware to fix cors

--- backend/index.js
+++ backend/index.js
@@ -14,12 +14,21 @@ server.use(cors({
   origin: process.env.FRONTEND_URL,
   methods: ["POST", "GET"]
 }));
+// Add middleware to enable CORS
+server.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', process.env.FRONTEND_URL);
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
 server.use(express.json());
 server.use(cookieParser());
 server.use("/auth",authRouter);
 server.use('/api/exercises', exerciseRoutes);
 
-  
+ 
+
 server.listen(process.env.PORT, () => {
     console.log("Server Started on port " + process.env.PORT);
     // console.log(process.env.FRONTEND_URL)
